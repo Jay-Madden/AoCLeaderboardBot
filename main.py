ef0fde1e8dcb2e214e4419b7a5d6e33483c49cf2
Jay Madden <jaymaddencox@gmail.com>
2021-12-01 13:59:26 -0500

    send global leaderboard

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import json
+import discord.ext.commands as commands
 
-import discord
+from leaderboard import LeaderboardCog
 
-with open('config.json.template') as f:
-    print('Loading Config')
-    config = json.load(f.read())
-    print('Config loaded')
-
-client = discord.Client()
-
+with open('config.json') as f:
+    print('Loading Config Values')
+    config = json.loads(f.read())
+    print('Config Values Loaded')
 
 
-client.Run()
+class AoCLeaderboardBot(commands.Bot):
+
+    def __init__(self, config, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.config = config
+
+    async def on_ready(self):
+        print('in on_ready')
+        self.load_extension('leaderboard')
+
+
+bot = AoCLeaderboardBot(config, command_prefix=None)
+bot.run(config['Token'])
