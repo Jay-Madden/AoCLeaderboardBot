23eaa730d75a1b5a14d376dda7921e3fdafcd0a2
Jay Madden <jaymaddencox@gmail.com>
2021-12-01 15:40:07 -0500

    Logging improvements

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import json
 import discord.ext.commands as commands
+import logging
 
-from leaderboard import LeaderboardCog
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger('bot')
+log.setLevel(logging.DEBUG)
 
 with open('config.json') as f:
-    print('Loading Config Values')
+    log.info('Loading Config Values')
     config = json.loads(f.read())
-    print('Config Values Loaded')
+    log.info('Config Values Loaded')
 
 
 class AoCLeaderboardBot(commands.Bot):
@@ -14,11 +17,16 @@ class AoCLeaderboardBot(commands.Bot):
     def __init__(self, config, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.config = config
+        self.load_extension('leaderboard')
+        log.info('Leaderboard Cog loaded')
 
     async def on_ready(self):
-        print('in on_ready')
-        self.load_extension('leaderboard')
+        log.info('Leaderboard Bot started up')
+
+    async def on_message(self, message):
+        pass
 
 
+log.info('Running Bot')
 bot = AoCLeaderboardBot(config, command_prefix=None)
 bot.run(config['Token'])
